f7e67de3e6de4840dbe1700216a306ed167fdec0
Zaki Asmat <zaki.asmat@sheridancollege.ca>
2025-05-30 16:37:10 -0400

    created a java file
    first revision

---
+++ Draw.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class Draw
+{
+    public static void main(String[] args)
+    {
+        Random rand  = new Random();
+        int    index = 1 + rand.nextInt(13);
+        System.out.println("Next student: " + index);
+    }
+}
